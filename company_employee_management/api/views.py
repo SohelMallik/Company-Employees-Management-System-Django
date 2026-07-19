1dd971a767da2209b258f1acac775e349ae836fc
Sohel Mallik <malliksohel582@gmail.com>
2026-07-19 23:16:06 +0530

    Remove authentication of Company

--- company_employee_management/api/views.py
+++ company_employee_management/api/views.py
@@ -19,7 +19,7 @@ class CompanyViewsset(viewsets.ModelViewSet):
     serializer_class=CompanySerializer
     
     #For Api Key Authentication
-    permission_classes=[HasAPIKey]
+    #permission_classes=[HasAPIKey]
     
     #companies/{pk}/employees/
     @action(detail=True, methods=['get'])
